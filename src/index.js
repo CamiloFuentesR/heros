bd56d97871d9a4a3bb0737034b825e23f3c779b8
Tutin R <lokomilo1@gmail.com>
2021-01-07 03:34:18 -0300

    charcter corto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { HeroesApp } from './HeroesApp';
 import  'animate.css'
 
 
 ReactDOM.render(
+    <StrictMode>
     <HeroesApp />
+    
+</StrictMode>
     ,
   document.getElementById('root')
 );
