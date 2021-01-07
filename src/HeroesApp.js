f238bccfdf69c5c439be525c8cef5331f2774619
Tutin R <lokomilo1@gmail.com>
2021-01-07 13:21:04 -0300

    charcter corto

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useEffect, useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
 import { AppRouter } from './routers/AppRouter'
+import { authReducer } from './auth/authReducer'
+
+
+//funcion para evalaur el local storege
+const init = () => {
+    //si existe retorn el storage y si no retorna logged false
+    return JSON.parse(localStorage.getItem('user')) || {
+        logged: false
+    };
+}
+
+
 
 export const HeroesApp = () => {
+    //                                   reducer,initialState
+    const [user, dispatch] = useReducer(authReducer, {}, init)
+
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user))
+    
+    }, [user]);
+
     return (
-        <>
-            <AppRouter/>
-        </>
+        //le estoy pasando el reducer a traves de un context a toda la aplicacion debido a que es un high order component
+        <AuthContext.Provider value={{user,dispatch}}>
+            <AppRouter />
+        </AuthContext.Provider>
 
     )
 }
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({history}) => {
 
+    const {dispatch} = useContext(AuthContext);
+
     const handleLogin = () => {
-        history.push('/');
-        // history.replace('/'); al estar autenticado no puedes volver al login a menos que presioes logout
+        // history.push('/');
+        // history.replace('/'); //al estar autenticado no puedes volver al login a menos que presioes logout
+        const user = {
+            name: 'Camilo F',
+        }
+        const action = {
+            type: types.login,
+            payload: user
+        }
+        dispatch(action);
+        history.replace('/');
+
     }
+    
 
     return (
         <div className="container">
